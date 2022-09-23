6bfcc6d04345f6b38911f820d7021f81f725fa46
Macmiller <58778886+MacmillerMartins@users.noreply.github.com>
2022-09-23 16:10:19 -0300

    ajustes finais

--- aks.tf
+++ aks.tf
@@ -19,7 +19,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
 resource "azurerm_role_assignment" "role_acrpull" {
   scope                = azurerm_container_registry.acr.id
   role_definition_name = "AcrPull"
-  principal_id         = "aks-bootcamp"
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  skip_service_principal_aad_check = true
 
   depends_on = [
   azurerm_container_registry.acr,
