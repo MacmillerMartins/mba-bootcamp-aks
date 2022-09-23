0112b1ecd8f10df00303f57f5eb0432cf3dee667
Macmiller <58778886+MacmillerMartins@users.noreply.github.com>
2022-09-23 16:18:29 -0300

    ajustes finais

--- aks.tf
+++ aks.tf
@@ -15,15 +15,3 @@ resource "azurerm_kubernetes_cluster" "aks" {
     type = "SystemAssigned"
   }
 }
-
-resource "azurerm_role_assignment" "role_acrpull" {
-  scope                = azurerm_container_registry.acr.id
-  role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-  skip_service_principal_aad_check = true
-
-  depends_on = [
-  azurerm_container_registry.acr,
-  azurerm_kubernetes_cluster.aks
-  ]
-}
