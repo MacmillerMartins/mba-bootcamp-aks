20b58940839c6f8a087007864766bc8656b005d9
Macmiller <58778886+MacmillerMartins@users.noreply.github.com>
2022-09-23 13:19:20 -0300

    ajustes

--- infra/main.tf
+++ infra/main.tf
@@ -1,9 +1,15 @@
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.15.00"
+    }
+  }
   backend "azurerm" {
-    resource_group_name  = "rg-bootcamp-tfstate"
+    resource_group_name = "rg-bootcamp-tfstate"
     storage_account_name = "sambabootcamptfstate"
-    container_name       = "scmbabootcamptfstate"
-    key                  = "terraform.tfstate"
+    container_name = "scmbabootcamptfstate"
+    key = "$(keyStorageAccount)"
   }
 }
 
